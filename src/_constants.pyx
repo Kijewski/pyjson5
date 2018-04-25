a6d1b184d8efdfe5f9de1b0c9d493b90a1c9c572
René Kijewski <rene.kijewski@fu-berlin.de>
2018-04-25 16:11:10 +0200

    .

--- src/_constants.pyx
+++ src/_constants.pyx
@@ -3,8 +3,8 @@ cdef object CONST_POS_INF = float('+Infinity')
 cdef object CONST_NEG_NAN = float('-NaN')
 cdef object CONST_NEG_INF = float('-Infinity')
 
-cdef object DATETIME_CLASSES = date, time, datetime
-cdef object ORD_CLASSES = (unicode, bytes, bytearray)
+cdef object DATETIME_CLASSES = (date, time, datetime,)
+cdef object ORD_CLASSES = (unicode, bytes, bytearray,)
 
 cdef object UCS1_COMPATIBLE_CODECS = frozenset((
     # ASCII
